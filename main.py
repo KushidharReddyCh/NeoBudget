3727487625e7ff028a01f34bb849a1e86dbcd694
Kushidhar <kushidhar@1.local>
2025-04-18 12:07:32 +0530

    added api handling

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {'data': {'name': 'John'}}
+@app.get("/blog")
+def index(limit: int = 123, published : bool = True, sort : Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} unpublished blogs from the db'}
+
+@app.get("/blog/unpublished")
+def get_unpublished_blogs():
+    return {'data': 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {'data': id}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    test = 1234,
+    body_1 = request.body
+    return {"data": f'blog is created with title as {request.title}'}
 
 
 
-@app.get("/about")
-def about():
-    return {'data': {'about page'}}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
